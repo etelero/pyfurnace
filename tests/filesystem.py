1de211d2e8e3ca77c2ff93d020b28d5adde12aa7
David D <D.T.Daschein@protonmail.com>
2018-06-27 12:59:14 +0300

    Refacor filesystem test using definitions list

--- tests/filesystem.py
+++ tests/filesystem.py
@@ -1,35 +1,39 @@
 import os
 
 err, ok = '[ERROR] ', '[OK] '
+errno = {
+    'interrupt': 1
+}
 
 fn = 'filesystem_test'
-f = open(fn, 'w')
-if not f:
-    print('Could not open file for writing')
-
 testf_lines = [
     'Aname,123,123',
     'Bname,456,456',
     'Dname,789,789',
 ]
 
-for i in testf_lines:
-    f.write(i + '\n')
-f.close()
 
-interrupt = 0
-f = open(fn, 'r')
-for i in testf_lines:
-    line = f.readline()[:-1]
-    if i != line:
-        interrupt = 1
-        print((err + 'Line missmatch: "{}" must be "{}"').format(line, i))
-        break
+def write():
+    f = open(fn, 'w')
+    if not f:
+        print(err + 'Could not open file for writing')
+        return(errno['interrupt'])
 
-if not interrupt:
+    for i in testf_lines:
+        f.write(i + '\n')
+    f.close()
+
+    f = open(fn, 'r')
+    for i in testf_lines:
+        line = f.readline()[:-1]
+        if i != line:
+            print((err + 'Line missmatch: "{}" must be "{}"').format(line, i))
+            return(errno['interrupt'])
     print(ok + 'File written succesfully')
     f.close()
 
+
+def remove():
     os.remove(fn)
     try:
         found = 1
@@ -39,5 +43,20 @@ if not interrupt:
     finally:
         if found:
             print(err + 'File deletion failed')
+            f.close()
         else:
             print(ok + 'File removed succesfully')
+
+
+defs = [
+    write,
+    remove,
+]
+
+
+res = 0
+for i in defs:
+    if res != 1:  # iterrupt
+        res = i()
+    else:
+        print('Interrupting test execution')
