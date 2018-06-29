e00530701c3e438a947a06c39d3fd7d540e0c68f
David D <D.T.Daschein@protonmail.com>
2018-06-29 17:33:37 +0300

    Implement and test Deletion and Insertion of a program

--- core/boot.py
+++ core/boot.py
@@ -1,4 +1,5 @@
 import ure
+import os
 
 # Setup ----------------------------------------------------------------------
 # This file is executed on every boot (including wake-boot from deepsleep)
@@ -33,14 +34,50 @@ class Storrage:
         self.programs = programs
         self.settings = settings
 
-    def __search_alpha_pos(self, name):
+    def __search_alpha_pos(self, name, f):
         line_number = 0
-        with open(self.programs, 'r') as openedf:
+        with open(f, 'r') as openedf:
             for line in openedf:
                 if name < line:
                     openedf.close()
-                    return line_number + 1
+                    return line_number
                 else:
                     line_number += 1
         openedf.close()
         return line_number
+
+    def insert_prog(self, program) -> int:
+        pf = self.programs
+        pos = 0
+        with open(pf, 'r') as pfo, open(pf + '_', 'w') as pfo_:
+            found = 0
+            for ln in pfo:
+                if program < ln and not found:
+                    found += 1
+                    pfo_.write(program + '\n')
+                    pfo_.write(ln)
+                else:
+                    pfo_.write(ln)
+                if not found:
+                    pos += 1
+            if not found:
+                pfo_.write(program + '\n')
+                # pos += 1
+        pfo.close()
+        pfo_.close()
+        os.remove(self.programs)
+        os.rename(self.programs + '_', self.programs)
+        return pos
+
+    def delete_prog(self, program):
+        pf = self.programs
+        with open(pf, 'r') as pfo, open(pf + '_', 'w') as pfo_:
+            for ln in pfo:
+                if str(ln).strip('\n') == program:
+                    pass
+                else:
+                    pfo_.write(ln)
+        pfo.close()
+        pfo_.close()
+        os.remove(self.programs)
+        os.rename(self.programs + '_', self.programs)
--- tests/filesystem.py
+++ tests/filesystem.py
@@ -3,6 +3,9 @@ from boot import Storrage
 
 err, ok = '[ERROR] ', '[OK] '
 
+verbose = False
+# verbose = True
+
 fn = 'filesystem_test'
 testf_lines = [
     'Aname,123,123',
@@ -13,10 +16,10 @@ testf_lines = [
     'Dname,789,789',
 ]
 searches = {
-    'Cname,456,456': 3,
-    'Cname111,456,456': 4,
-    'Cname2000,456,457': 6,
-    'Cname2_1,456,456': 6,
+    'Cname,456,456': 2,
+    'Cname111,456,456': 3,
+    'Cname2000,456,457': 5,
+    'Cname2_1,456,456': 5,
 }
 
 S = Storrage(fn)
@@ -46,7 +49,7 @@ def write() -> int:
 def search_alpha_pos() -> int:
     failed = 0
     for line in searches:
-        pos = S.__search_alpha_pos(line)
+        pos = S.__search_alpha_pos(line, S.programs)
         if pos != searches[line]:
             print(
                 err
@@ -63,26 +66,84 @@ def search_alpha_pos() -> int:
         return 0
 
 
-def remove() -> int:
-    os.remove(fn)
+def remove(filename=fn, active=1) -> int:
+    if active:
+        os.remove(filename)
     try:
         found = 1
-        f = open(fn, 'r')
+        f = open(filename, 'r')
     except OSError:
         found = 0
     finally:
         if found:
             print(err + 'File deletion failed')
             f.close()
+            return 0
         else:
             print(ok + 'File removed succesfully')
     return 1
 
 
+def insert_delete_remove() -> int:
+    progs = {
+        '1name,324,345,342': 0,
+        'Cname,456,456': 2,
+        'Zname,324,345,342': 6,
+    }
+    failed = 0
+    for prog in progs:
+        pos = S.insert_prog(prog)
+        if pos != progs[prog]:
+            print(
+                err + "Insertion at wrong pos."
+                "Pos == {} expected {}".format(pos, progs[prog])
+            )
+            failed += 1
+        if remove(fn + '_', 0):
+            print(ok + "Directory is clean")
+        else:
+            print(err + "Directory is dirty")
+            return 0
+
+        if verbose:
+            print('\nAfter insert:')
+            f = open(fn, 'r')
+            for l in f:
+                print(l.strip('\n'))
+            f.close()
+
+        S.delete_prog(prog)
+        if verbose:
+            print('\nAfter del:')
+            f = open(fn, 'r')
+            for l in f:
+                print(l.strip('\n'))
+            f.close()
+
+        f = open(fn, 'r')
+        del_test_iter = 0
+        for fline in f:
+            try:
+                if str(fline).strip('\n') != testf_lines[del_test_iter]:
+                    print(err + "Deletion failed")
+                    failed += 1
+                    break
+            except IndexError:
+                print(err + 'File length missmatch')
+                failed += 1
+                break
+            del_test_iter += 1
+
+        f.close()
+
+    if not failed:
+        print(ok + "Inserted and deleted progs succesfully")
+
+
 defs = [
     write,
     search_alpha_pos,
-    remove,
+    insert_delete_remove,
 ]
 
 if S.programs in os.listdir():
